2a7cfb2837aba0b267a5cf75e0d90aa0ee2d446c
Lathiya Harsh <62006931+mrlathiya@users.noreply.github.com>
2023-04-12 18:16:48 -0400

    Completed Working Application
    -Added adapter to main activity
    -Work flow Completed

--- app/src/main/java/com/precioso/inclass_5/ProductAdapter.java
+++ app/src/main/java/com/precioso/inclass_5/ProductAdapter.java
@@ -1,9 +1,9 @@
 package com.precioso.inclass_5;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,63 +12,37 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHolder> {
+public class ProductAdapter extends RecyclerView.Adapter<MyViewHolder> {
 
 
-    private ArrayList<ProductModel> productsList;
+    Context context;
+    List<ProductModel> productsList;
 
-    public ProductAdapter(ArrayList<ProductModel> products) {
-        this.productsList = products;
+    public ProductAdapter(Context context, List<ProductModel> productsList){
+        this.context = context;
+        this.productsList = productsList;
     }
-
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = (View) LayoutInflater.from(parent.getContext()).inflate(R.layout.productcard, parent, false);
-
-        return new ViewHolder(v);
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new MyViewHolder(LayoutInflater.from(context).inflate(R.layout.productcard,parent,false));
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        ProductModel products = productsList.get(position);
-        holder.name.setText(products.getName());
-        holder.price.setText(products.getPrice());
-        holder.id.setText(products.getId());
-//        Picasso.get().load(products.getImage()).into(holder.image);
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.name.setText(productsList.get(position).getName());
+        holder.id.setText(productsList.get(position).getName());
+        holder.price.setText(productsList.get(position).getName());
+        Picasso.get().load(productsList.get(position).getImage()).into(holder.imageView);
     }
-
 
 
     @Override
     public int getItemCount() {
-        if(list != null){
-            return list.size();
-        }else {
-            return 0;
-        }
-    }
-
-    ArrayList<ProductModel> list;
-
-
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        public final View view;
-        public final TextView name;
-        public final TextView id;
-        public final TextView price;
-//        public final ImageView image;
-
-        public ViewHolder(View view) {
-            super(view);
-            this.view = view;
-            name = view.findViewById(R.id.name);
-            price = view.findViewById(R.id.price);
-//            image = view.findViewById(R.id.image);
-            id = view.findViewById(R.id.id);
-        }
+            return productsList.size();
     }
 }
 
